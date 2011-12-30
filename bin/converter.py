2f4dc83660c34b369e927b8703a854f35b3498d4
Tualatrix Chou <tualatrix@gmail.com>
2011-12-30 10:54:24 +0800

    Add a converter to conver picture for imtx

---
+++ bin/converter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+import glob
+
+to_converted = glob.glob('*.JPG')
+to_converted.sort()
+
+print("Please enter the prefix: %s")
+prefix = raw_input()
+
+if not os.path.exists(prefix):
+    os.makedirs(prefix)
+
+for i, name in enumerate(to_converted):
+    print('convert -resize 550x413 %s %s/%s-%d' % (name, prefix, prefix, i))
+    os.system('convert -resize 550x413 %s %s/%s-%d' % (name, prefix, prefix, i))
